2f31798ba7eb90a6470765670542b74acab2ae10
Jovakinn <max05012004@gmail.com>
2024-09-11 20:24:41 +0300

    init commit

---
+++ webs/src/main/java/mo/khodakov/webs/WebsApplication.java
@@ -0,0 +1,13 @@
+package mo.khodakov.webs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsApplication.class, args);
+    }
+
+}
---
+++ webs/src/test/java/mo/khodakov/webs/WebsApplicationTests.java
@@ -0,0 +1,13 @@
+package mo.khodakov.webs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
