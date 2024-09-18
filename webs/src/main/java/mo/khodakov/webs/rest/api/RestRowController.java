e40c711455d3fcf361cd894def8941f5172f1ee1
Jovakinn <max05012004@gmail.com>
2024-09-18 22:21:19 +0300

    refactor REST API

--- webs/src/main/java/mo/khodakov/webs/rest/api/RestRowController.java
+++ webs/src/main/java/mo/khodakov/webs/rest/api/RestRowController.java
@@ -14,36 +14,36 @@ import org.springframework.web.bind.annotation.*;
 public class RestRowController {
     private final DatabaseService databaseService;
 
-    @GetMapping(value = "/rows/{tableName}/select/{columns}/{condition}")
+    @GetMapping(value = "/rows/select-by-conditions")
     @Operation(description = "Select by conditions from DB")
-    public ResponseEntity<Result> selectCondition(@PathVariable String columns,
-                                                  @PathVariable String tableName,
-                                                  @PathVariable String condition) throws ApiException {
+    public ResponseEntity<Result> selectCondition(@RequestParam String columns,
+                                                  @RequestParam String tableName,
+                                                  @RequestParam String condition) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("select %s from %s where %s", columns, tableName, condition)));
     }
 
-    @GetMapping(value = "/rows/{tableName}/select/{columns}")
+    @GetMapping(value = "/rows/select")
     @Operation(description = "Select from DB without conditions")
-    public ResponseEntity<Result> select(@PathVariable String columns,
-                                         @PathVariable String tableName) throws ApiException {
+    public ResponseEntity<Result> select(@RequestParam String columns,
+                                         @RequestParam String tableName) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("select %s from %s", columns, tableName)));
     }
 
-    @PostMapping(value = "/rows/{tableName}/insert/{columns}/{values}")
+    @PostMapping(value = "/rows/insert")
     @Operation(description = "Insert into table")
-    public ResponseEntity<Result> insert(@PathVariable String columns,
-                                         @PathVariable String tableName,
-                                         @PathVariable String values) throws ApiException {
+    public ResponseEntity<Result> insert(@RequestParam String columns,
+                                         @RequestParam String tableName,
+                                         @RequestParam String values) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("insert into %s (%s) values (%s)", tableName, columns, values)));
     }
 
-    @DeleteMapping(value = "/rows/{tableName}/delete/{condition}")
+    @DeleteMapping(value = "/rows/delete")
     @Operation(description = "Delete from DB by condition")
-    public ResponseEntity<Result> delete(@PathVariable String tableName,
-                                         @PathVariable String condition) throws ApiException {
+    public ResponseEntity<Result> delete(@RequestParam String tableName,
+                                         @RequestParam String condition) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("delete from %s where %s", tableName, condition)));
     }
--- webs/src/main/java/mo/khodakov/webs/rest/api/RestTableController.java
+++ webs/src/main/java/mo/khodakov/webs/rest/api/RestTableController.java
@@ -21,34 +21,34 @@ public class RestTableController {
         return ResponseEntity.ok(database.query("list tables"));
     }
 
-    @DeleteMapping(value = "/tables/delete/{tableName}")
+    @DeleteMapping(value = "/tables/delete")
     @Operation(description = "Drop table in DB")
-    public ResponseEntity<Result> dropTable(@PathVariable String tableName) throws ApiException {
+    public ResponseEntity<Result> dropTable(@RequestParam String tableName) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("remove table %s", tableName)));
     }
 
-    @PostMapping(value = "/tables/create/{tableName}/{columns}")
+    @PostMapping(value = "/tables/create")
     @Operation(description = "Create table in DB")
-    public ResponseEntity<Result> createTable(@PathVariable String columns,
-                                              @PathVariable String tableName) throws ApiException {
+    public ResponseEntity<Result> createTable(@RequestParam String columns,
+                                              @RequestParam String tableName) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("create table %s (%s)", tableName, columns)));
     }
 
-    @PostMapping(value = "/tables/{tableLeftName}/combine/{tableRightName}")
+    @PostMapping(value = "/tables/combine")
     @Operation(description = "Combine tables in DB")
-    public ResponseEntity<Result> combine(@PathVariable String tableLeftName,
-                                          @PathVariable String tableRightName) throws ApiException {
+    public ResponseEntity<Result> combine(@RequestParam String tableLeftName,
+                                          @RequestParam String tableRightName) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("combine %s with %s", tableLeftName, tableRightName)));
     }
 
 
-    @PostMapping(value = "/tables/{tableLeftName}/subtract/{tableRightName}")
+    @PostMapping(value = "/tables/subtract")
     @Operation(description = "Subtract tables in DB")
-    public ResponseEntity<Result> subtract(@PathVariable String tableLeftName,
-                                           @PathVariable String tableRightName) throws ApiException {
+    public ResponseEntity<Result> subtract(@RequestParam String tableLeftName,
+                                           @RequestParam String tableRightName) throws ApiException {
         final var database = databaseService.getDatabase();
         return ResponseEntity.ok(database.query(String.format("subtract %s from %s", tableLeftName, tableRightName)));
     }
