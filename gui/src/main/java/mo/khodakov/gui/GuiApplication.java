e16f2fb16aedf776da8a34fbf480b5e941174b65
Jovakinn <max05012004@gmail.com>
2024-09-20 22:33:12 +0300

    init GUI

--- gui/src/main/java/mo/khodakov/gui/GuiApplication.java
+++ gui/src/main/java/mo/khodakov/gui/GuiApplication.java
@@ -3,6 +3,9 @@ package mo.khodakov.gui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * we need this class to import classes to Webs module
+ */
 @SpringBootApplication
 public class GuiApplication {
 
---
+++ gui/src/main/java/mo/khodakov/gui/actualui/UIStarter.java
@@ -0,0 +1,7 @@
+package mo.khodakov.gui.actualui;
+
+public class UIStarter {
+    public static void main(String[] args) {
+        new DBGUIManager().setVisible(true);
+    }
+}
