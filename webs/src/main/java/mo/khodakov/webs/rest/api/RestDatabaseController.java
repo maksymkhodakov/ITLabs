df46633ffba65120fff19adfffe063450545282c
Jovakinn <max05012004@gmail.com>
2024-09-21 19:33:09 +0300

    add create DB from scratch to REST API

--- webs/src/main/java/mo/khodakov/webs/rest/api/RestDatabaseController.java
+++ webs/src/main/java/mo/khodakov/webs/rest/api/RestDatabaseController.java
@@ -3,6 +3,7 @@ package mo.khodakov.webs.rest.api;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.extern.slf4j.Slf4j;
 import mo.khodakov.gui.database.Database;
 import mo.khodakov.gui.database.DatabaseReader;
 import mo.khodakov.webs.rest.exceptions.ApiException;
@@ -11,18 +12,50 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Collection;
 
 
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class RestDatabaseController {
     public static final String DOWNLOAD_DEFAULT_FILENAME = "DB.json";
     private Database database;
 
+    @PostMapping("/database/create")
+    @Operation(description = "Method for initial create DB upload")
+    public ResponseEntity<Void> createDatabase(@RequestParam String path) {
+        if (path == null) {
+            throw new ApiException(ErrorCode.PATH_NOT_FOUND);
+        }
+
+        if (!path.endsWith(".json")) {
+            path += ".json";
+        }
+
+        File newDatabaseFile = new File(path);
+
+        if (!newDatabaseFile.exists()) {
+            try {
+                newDatabaseFile.createNewFile();
+                // Ініціалізація нової бази даних та запис її в файл.
+                this.database = new Database(path);
+                this.database.save();
+                this.database = new DatabaseReader(path).read();
+                return ResponseEntity.ok().build();
+            } catch (IOException e) {
+                throw new ApiException(ErrorCode.FILE_CREATION_FAILED);
+            }
+        } else {
+            throw new ApiException(ErrorCode.FILE_ALREADY_EXISTS);
+        }
+    }
+
     @PostMapping("/database/upload")
-    @Operation(description = "Method for initial DB upload")
+    @Operation(description = "Method for initial existing DB upload")
     public ResponseEntity<Collection<String>> uploadDatabase(@RequestParam String path) {
         try {
             database = new DatabaseReader(path).read();
--- webs/src/main/java/mo/khodakov/webs/rest/exceptions/ErrorCode.java
+++ webs/src/main/java/mo/khodakov/webs/rest/exceptions/ErrorCode.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     NO_ACTIVE_DATABASE("No active database"),
     FILE_NOT_FOUND("File not found"),
-    INVALID_JSON("Invalid JSON");
+    INVALID_JSON("Invalid JSON"),
+    PATH_NOT_FOUND("Filepath not found"),
+    FILE_CREATION_FAILED("File creation failed"),
+    FILE_ALREADY_EXISTS("File already exists");
     private final String data;
 }
